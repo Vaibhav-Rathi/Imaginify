2585f1f08d7a07487c82a6baa5ac1df34fdcb91a
Vaibhav-Rathi <vaibhavvaibhav6@gmail.com>
2024-10-09 15:35:16 +0530

    Final commit for code

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -52,12 +52,14 @@ const TransformedImage = ({ image, type, title, transformationConfig, isTransfor
             placeholder={dataUrl as PlaceholderValue}
             className="transformed-image"
             onLoad={() => {
-              setIsTransforming && setIsTransforming(false);
+              if (setIsTransforming) {
+                setIsTransforming(false);
+              }              
             }}
             onError={() => {
               debounce(() => {
-                setIsTransforming && setIsTransforming(false);
-              }, 8000)()
+                if (setIsTransforming) setIsTransforming(false);
+              }, 8000);              
             }}
             {...transformationConfig}
           />
